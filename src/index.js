bbe270c5860d5e4f5b82e6a47c86f41295a69654
Paweł Brzozowski <pawel@brzoza.net>
2023-04-29 22:36:19 +0200

    Fix fetchCountries function

--- src/index.js
+++ src/index.js
@@ -5,11 +5,38 @@ import fetchCountries from "./js/fetchCountries";
 
 const DEBOUNCE_DELAY = 300;
 const searchEl = document.querySelector("#search-box");
-const countryListEl = document.querySelector("#country-list");
-const countryInfoEl = document.querySelector("#country-info");
+const countryListEl = document.querySelector(".country-list");
+const countryInfoEl = document.querySelector(".country-info");
+
+const displayCountry = country => {
+  const { name, capital, population, languages, flags } = country;
+  const languagesList = languages.map(language => language.name).join(", ");
+  const countryInfo = `
+    <div class="country-info__flag">
+      <img src="${flags.png}" alt="${name.common}" width="300">
+    </div>
+    <div class="country-info__text">
+      <h2 class="country-info__name">${name.common}</h2>
+      <p class="country-info__capital"><b>Capital:</b> ${capital}</p>
+      <p class="country-info__population"><b>Population:</b> ${population}</p>
+      <p class="country-info__languages"><b>Languages:</b> ${languagesList}</p>
+    </div>
+  `;
+  countryInfoEl.innerHTML = countryInfo;
+  console.log(countryInfo);
+};
+
+displayCountry({
+  name: { common: "Poland" },
+  capital: "Warsaw",
+  population: 38413000,
+  languages: [{ name: "Polish" }],
+  flags: { png: "https://flagcdn.com/w320/pl.png" },
+});
+
 
 const debouncedFunction = debounce(event => {
-  const searchQuery = event.target.value;
+  const searchQuery = event.target.value.trim();
   console.log(searchQuery);
 }, DEBOUNCE_DELAY);
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,16 @@
 import Notiflix from "notiflix";
 
-const API_URL =
-  "https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages";
+const API_URL = "https://restcountries.com/v3.1/name/";
+const FILTERS = "?fields=name,capital,population,flags,languages";
 
-export function fetchCountries() {
-  fetch(API_URL)
+export function fetchCountries(countryName) {
+  fetch(`${API_URL}${countryName}${FILTERS}`)
     .then(response => response.json())
     .then(data => console.log(data))
-    .catch(error => Notiflix.Notify.failure("Oops, error fetching data from server"));
+    .catch(error =>
+      Notiflix.Notify.failure("Oops, error fetching data from server")
+    );
 }
 
-fetchCountries();
+fetchCountries("sw");
+fetchCountries("Poland");
