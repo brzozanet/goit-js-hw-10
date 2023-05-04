daab4ff48fcf0262fbde155df15f0a46a585ccba
Paweł Brzozowski <pawel@brzoza.net>
2023-05-04 12:21:38 +0200

    Fix fetchCountries function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Notiflix from "notiflix";
 import debounce from "lodash/debounce";
 import { fetchCountries } from "./js/fetchCountries.js";
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
 const searchEl = document.querySelector("#search-box");
 const countryListEl = document.querySelector(".country-list");
 const countryInfoEl = document.querySelector(".country-info");
@@ -26,43 +26,46 @@ const displayCountry = country => {
   console.log(countryInfo);
 };
 
-searchEl.addEventListener("input", debounce(event => {
-  const searchQuery = event.target.value.trim();
-  if (searchQuery.length === 0) {
-    countryListEl.innerHTML = "";
-    countryInfoEl.innerHTML = "";
-    return;
-  }
-  fetchCountries(searchQuery)
-    .then(countries => {
-      if (countries.length > 10) {
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-        countryListEl.innerHTML = "";
-        countryInfoEl.innerHTML = "";
-        return;
-      }
-      if (countries.length > 1) {
-        const countriesList = countries.map(country => `<li>${country.name.common}</li>`).join("");
-        countryListEl.innerHTML = countriesList;
-        countryInfoEl.innerHTML = "";
-        return;
-      }
-      if (countries.length === 1) {
-        displayCountry(countries[0]);
-        countryListEl.innerHTML = "";
-        return;
-      }
-      Notiflix.Notify.failure("Oops, error fetching data from server");
+searchEl.addEventListener(
+  "input",
+  debounce(event => {
+    const searchQuery = event.target.value.trim();
+    if (searchQuery.length === 0) {
       countryListEl.innerHTML = "";
       countryInfoEl.innerHTML = "";
-    })
-    .catch(error => {
-      Notiflix.Notify.failure("Oops, error fetching data from server");
-      countryListEl.innerHTML = "";
-      countryInfoEl.innerHTML = "";
-    });
-}, DEBOUNCE_DELAY));
-
-fetchCountries("Po");
-fetchCountries("Poland");
-
+      return;
+    }
+    fetchCountries(searchQuery)
+      .then(countries => {
+        if (countries.length > 10) {
+          Notiflix.Notify.info(
+            "Too many matches found. Please enter a more specific name."
+          );
+          countryListEl.innerHTML = "";
+          countryInfoEl.innerHTML = "";
+          return;
+        }
+        if (countries.length > 1) {
+          const countriesList = countries
+            .map(country => `<li>${country.name.common}</li>`)
+            .join("");
+          countryListEl.innerHTML = countriesList;
+          countryInfoEl.innerHTML = "";
+          return;
+        }
+        if (countries.length === 1) {
+          displayCountry(countries[0]);
+          countryListEl.innerHTML = "";
+          return;
+        }
+        // Notiflix.Notify.failure("Oops, error fetching data from server");
+        // countryListEl.innerHTML = "";
+        // countryInfoEl.innerHTML = "";
+      })
+      .catch(error => {
+        Notiflix.Notify.failure("Oops, error fetching data from server");
+        countryListEl.innerHTML = "";
+        countryInfoEl.innerHTML = "";
+      });
+  }, DEBOUNCE_DELAY)
+);
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,13 +4,10 @@ const API_URL = "https://restcountries.com/v3.1/name/";
 const FILTERS = "?fields=name,capital,population,flags,languages";
 
 export function fetchCountries(countryName) {
-  fetch(`${API_URL}${countryName}${FILTERS}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error =>
-      Notiflix.Notify.failure("Oops, error fetching data from server")
-    );
+  return fetch(`${API_URL}${countryName}${FILTERS}`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.status);
+  });
 }
-
-// fetchCountries("sw");
-// fetchCountries("Poland");
